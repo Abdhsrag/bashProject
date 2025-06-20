9e1ab3bb68e68a3617102f0359e300e4b999342d
Abdelrahman mohamed <abdhsrag280@gmail.com>
2025-06-20 17:04:01 +0300

    made the init.sh and main.sh

---
+++ main.sh
@@ -0,0 +1,24 @@
+operations=("Create database" "Drop database" "List databases" "Connect to database" "Exit")
+select var in "${operations[@]}"; do
+    case $var in
+	"Connect to database")
+	    source $scriptLocation/operations/connect.sh
+	    ;;
+	"Create database")
+	    source $scriptLocation/operations/create.sh
+	    ;;
+	"Drop database")
+	    source $scriptLocation/operations/drop.sh
+	    ;;
+	"List databases")
+	    source $scriptLocation/operations/list.sh
+	    ;;
+	"Exit")
+	    echo "Closing the app..."
+	    break
+	    ;;
+	*)
+	    echo "Invalid option!"
+	    ;;
+    esac
+done
